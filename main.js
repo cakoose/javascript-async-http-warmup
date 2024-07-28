922aa8dc387340ee63cb3273d12cc8eaf53768ff
Kannan Goundan <kannan@anrok.com>
2024-07-28 16:38:40 -0700

    For online REPL, point to runjs.co instead of now-defunct anyfiddle.com
    Why runjs.co?
    - The code runs correctly. Many others don't work with HTTP requests.
    - No login required.
    - Simple interface, e.g. not HTML/CSS, no file browser, etc.
    
    Thanks for @ronzilla for pointing out that Anyfiddle has shut down.

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 //    'httpGetSerial' with some test URLs.  The expected output
 //    is shown at the very bottom of this file.
 //
-// You can run this on your computer with Node.js or use an online Node.js
-// environment, e.g. https://www.anyfiddle.com/templates/nodejs/
+// You can run this on your computer with Node.js or use an online JavaScript
+// environment that allows HTTP requests, e.g. https://runjs.co/
 
 // A callback-style function to make an HTTP GET request.
 // When the request is complete, 'callback' will be called with
